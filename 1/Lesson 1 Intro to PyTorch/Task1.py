6994a261b876c5974e7e671d582d664a53eb66c4
Danila Baranov <bara.danila2010@gmail.com>
2022-08-30 15:14:25 +0300

    Modify DL Folder

--- 1/Lesson 1 Intro to PyTorch/Task1.py
+++ 1/Lesson 1 Intro to PyTorch/Task1.py
@@ -3,9 +3,9 @@ import torch
 
 def function01(tensor: torch.tensor, count_over: str):
     if count_over == 'columns':
-        return torch.mean(tensor, dim=1)
+        return tensor.mean(dim=0)
 
     if count_over == 'rows':
-        return torch.mean(tensor, dim=0)
+        return tensor.mean(dim=1)
 
     return None
---
+++ 1/Lesson 1 Intro to PyTorch/Task2.py
@@ -0,0 +1,5 @@
+import torch
+
+
+def function02():
+
