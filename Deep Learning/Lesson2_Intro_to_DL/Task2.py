251860403d7f3f60d08f6e76fc3303887ddffe16
Danila Baranov <bara.danila2010@gmail.com>
2022-09-01 22:38:54 +0300

    Add Task 2

---
+++ Deep Learning/Lesson2_Intro_to_DL/Task2.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn, optim
+from torch.utils.data import DataLoader
+
+
+def train(model: nn.Module, data_loader: DataLoader, optimizer: optim.Optimizer, loss_fn):
+    model.train()
+    total_loss = 0
+    for x, y in data_loader:
+        optimizer.zero_grad()
+        output = model(x)
+
+        loss = loss_fn(output, x)
+        loss.backward()
+        print(round(loss, 5))
+
+        optimizer.step()
+
+        total_loss += loss
+
+    total_loss /= len(data_loader)
+
+    return total_loss
