02d345b3ed34ea27a6314ea84408f7af54accc21
Danila Baranov <bara.danila2010@gmail.com>
2022-09-05 19:41:09 +0300

    Add Task 2

---
+++ Deep Learning/Lesson3_Conv_nn_Part1/Task2.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import torch
+import torch.nn as nn
+import torchvision.transforms as T
+from IPython.display import clear_output
+from PIL import Image
+from matplotlib import cm
+from time import perf_counter
+from torch.utils.data import DataLoader
+from torchvision.datasets import MNIST
+from tqdm import tqdm
+
+
+
+
