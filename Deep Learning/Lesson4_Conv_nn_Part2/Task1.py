6d4d950bc032ea19f54d0716076a3d1ca6da1540
Danila Baranov <bara.danila2010@gmail.com>
2022-09-09 17:28:40 +0300

    Modify Task 1

--- Deep Learning/Lesson4_Conv_nn_Part2/Task1.py
+++ Deep Learning/Lesson4_Conv_nn_Part2/Task1.py
@@ -1,7 +1,17 @@
 import torch
+from torchvision.datasets import CIFAR10
+from torchvision import transforms as T
 
 
 def get_normalize(features: torch.Tensor):
-    means = features.mean(axis=(0, 2, 3))
-    stds = features.std(axis=(0, 2, 3))
+    means = features.data.mean(axis=(0, 1, 2))
+    stds = features.data.std(axis=(0, 1, 2))
     return means, stds
+
+
+train_data = CIFAR10('./data', train=True, transform=T.ToTensor(), download=True)
+test_data = CIFAR10('./data', train=False, transform=T.ToTensor(), download=True)
+
+means, stds = get_normalize(train_data)
+print(f'Means: {means}')
+print(f'Stds: {stds}')
