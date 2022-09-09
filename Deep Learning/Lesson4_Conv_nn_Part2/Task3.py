1581b66164477cfe558d8f95900e07946e64d553
Danila Baranov <bara.danila2010@gmail.com>
2022-09-09 19:51:26 +0300

    Add Solved Task 3

--- Deep Learning/Lesson4_Conv_nn_Part2/Task3.py
+++ Deep Learning/Lesson4_Conv_nn_Part2/Task3.py
@@ -13,7 +13,6 @@ def predict(model: nn.Module, loader: DataLoader, device: torch.device):
     for x, y in loader:
         x, y = x.to(device), y.to(device)
         output = model(x)
-        preds.append(output[torch.argmax(output, dim=1)])
+        preds.append(torch.argmax(output, dim=1))
 
-    preds = torch.cat(preds)
-    return torch.tensor(preds, dtype=torch.int)
+    return torch.cat(preds)
